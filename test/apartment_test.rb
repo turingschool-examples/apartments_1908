3878592a8422b32f0b58365c909a289dcd0c258d
Laura Schulz <lrs8810@gmail.com>
2019-08-28 09:11:10 -0600

    Initialize renter class & add all files

---
+++ test/apartment_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require 'pry'
+
+class ApartmentTest < Minitest::Test
---
+++ test/renter_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require 'pry'
+
+class RenterTest < Minitest::Test
+
+def setup
+  @renter = Renter.new("Jessie")
+end
+
+def test_it_exits
+  assert_instance_of Renter, @renter
+end
+
+def test_it_has_attributes
+  assert_equal "Jessie", @renter.name 
+end
+end
