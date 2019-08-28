2e21b6548c6e536b206a0b2a12b1e5bc4f30a52a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-28 09:38:04 -0600

    Add test for attributes in Building test

--- test/building_test.rb
+++ test/building_test.rb
@@ -4,6 +4,10 @@ require './lib/building'
 
 class BuildingTest < Minitest::Test
   def setup
-    building = Building.new
+    @building = Building.new
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @building.units
   end
 end
