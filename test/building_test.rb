ac6ffc1a5a7aa417a2b5b3ad91e7bad776ed0067
Allison E. Moore <m3gacrypt0@gmail.com>
2019-08-28 09:34:14 -0600

    Add file building_test.rb

---
+++ test/building_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+
+class BuildingTest < Minitest::Test
+
+  def setup
+    @building = Building.new
+  end
+
+  def test_it_exists
+    assert_instance_of Building, @building
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @building.units #initial attribute
+  end
+
+  def test_method_add_unit
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @building.add_unit(@a1)
+    assert_equal [@a1], @building.units
+
+    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(@b1)
+    assert_equal [@a1, @b1], @building.units
+
+    assert_instance_of Apartment, @building.units[0]
+    assert_instance_of Apartment, @building.units[1]
+  end
+
+  def test_method_average_rent
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @building.add_unit(@a1)
+
+    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(@b1)
+
+    assert_equal 1099.5, @building.average_rent
+  end
+
+end
