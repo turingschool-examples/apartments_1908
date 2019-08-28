448182a9c7e0966b0f1de1b594e133f178f83a7a
Rachel Lew <rlew07@gmail.com>
2019-08-28 09:24:57 -0600

    Adds apartment class and apartment test

---
+++ lib/apartment.rb
@@ -0,0 +1,16 @@
+class Apartment
+
+attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
+
+  def initialize(apartment)
+    @number = apartment[:number]
+    @monthly_rent = apartment[:monthly_rent]
+    @bathrooms = apartment[:bathrooms]
+    @bedrooms = apartment[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter = renter
+  end
+end
---
+++ test/apartment_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+
+class ApartmentTest < Minitest::Test
+
+  def setup
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  def test_it_exists
+    assert_instance_of Apartment, @a1
+  end
+
+  def test_it_has_attributes
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+    assert_nil @a1.renter
+  end
+
+  def test_it_can_add_renters
+    jessie = Renter.new("Jessie")
+
+    @a1.add_renter(jessie)
+    assert_equal jessie, @a1.renter
+  end
+end
