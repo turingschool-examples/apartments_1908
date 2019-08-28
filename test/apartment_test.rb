b98d956bfde6c7dc2ed34742947c343b91321178
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-28 09:13:02 -0600

    Add Apartment test file

---
+++ test/apartment_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/apartment'
+
+
+class ApartmentTest < Minitest::Test
+  def setup
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+  end
+
+  def test_it_exists
+    assert_instance_of Apartment, @a1
+  end
+
+
+
+
+end
