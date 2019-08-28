3300352f7a1331996b615c44ef946ac1dafb95fd
Scott Regenthal <scottregenthal@gmail.com>
2019-08-28 10:58:36 -0600

    Add building class and test – incomplete

--- lib/building.rb
+++ lib/building.rb
@@ -5,5 +5,19 @@ class Building
     @units = []
   end
 
+  def add_unit(unit)
+    @units.push(unit)
+  end
+
+  def units
+  end
+
+  def average_rent
+    @building.units
+  end
+
+  def renter_with_highest_rent
+
+  end
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -8,14 +8,46 @@ class BuildingTest < Minitest::Test
 
   def setup
     @building = Building.new
+
+    @a1 = Apartment.new({number: "A1",
+                         monthly_rent: 1200,
+                         bathrooms: 1,
+                         bedrooms: 1})
+    @b2 = Apartment.new({number: "B2",
+                         monthly_rent: 999,
+                         bathrooms: 2,
+                         bedrooms: 2})
+
+    @spencer = Renter.new("Spencer")
+    @jessie = Renter.new("Jessie")
+
+    @b2.add_renter(@spencer)
+    @a1.add_renter(@jessie)
+
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
   end
 
   def test_it_exists
     assert_instance_of Building, @building
   end
 
-  def test_it_has_an_empty_array_of_units
+  def test_it_starts_with_an_empty_array_of_units
     assert_equal [], @building.units
+  end
+
+  def test_it_can_add_units
+    assert_equal [@a1], @building.add_unit(@a1)
+    assert_equal [@a1, @a2], @building.add_unit(@a2)
+    assert_equal [@a1, @a2], @building.units
+  end
+
+  def test_it_can_return_average_rent
+    assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_it_can_access_renter_by_highest_rent
+    assert_equal @jessie, @building.renter_with_highest_rent
   end
 
 end
