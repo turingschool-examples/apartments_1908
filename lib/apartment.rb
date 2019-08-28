27e00b2abd5c3684fdbfe3b2d50b99ce3000f3cc
Scott Regenthal <scottregenthal@gmail.com>
2019-08-28 10:56:46 -0600

    Add apartment class and test

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,2 +1,20 @@
 class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
+  def initialize(number:, monthly_rent:, bathrooms:, bedrooms:)
+    @number = number
+    @monthly_rent = monthly_rent
+    @bathrooms = bathrooms
+    @bedrooms = bedrooms
+    @renter = nil
+  end
+
+  def add_renter(renter)
+    @renter(renter)
+  end
+
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -3,7 +3,32 @@ require 'minitest/pride'
 require './lib/renter'
 require './lib/apartment'
 
-def ApartmentTest < Minitest::Test
+class ApartmentTest < Minitest::Test
 
+  def setup
+    @a1 = Apartment.new({number: "A1",
+                         monthly_rent: 1200,
+                         bathrooms: 1,
+                         bedrooms: 1})
+    @jessie = Renter.new("Jessie")
+  end
 
+  def test_it_exists
+    assert_instance_of Apartment, @a1
+  end
+
+  def test_attributes
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+  end
+
+  def test_it_starts_without_a_renter
+    assert_nil @a1.renter
+  end
+
+  def test_it_can_add_a_renter
+    assert_equal @jessie, @a1.add_renter(@jessie)
+  end
 end
