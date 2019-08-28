4d5ec15fbc28529a1f24c33768b39d3f0f34e643
Scott Regenthal <scottregenthal@gmail.com>
2019-08-28 10:57:56 -0600

    Complete renter class and test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -1,8 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/renter'
+require './lib/apartment'
 
-def RenterTest < Minitest::Test
+class RenterTest < Minitest::Test
 
+  def setup
+    @jessie = Renter.new("Jessie")
+  end
+
+  def test_it_exists
+    assert_instance_of Renter, @jessie
+  end
+
+  def test_it_has_a_name
+    assert_equal "Jessie", @jessie.name
+  end
 
 end
