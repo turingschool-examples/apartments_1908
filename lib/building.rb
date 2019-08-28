d8c9291f2f6b758c464830f1ccf0915d2f776e6f
Rachel Lew <rlew07@gmail.com>
2019-08-28 10:59:40 -0600

    Adds renter_with_highest_rent method and test

--- lib/building.rb
+++ lib/building.rb
@@ -15,4 +15,23 @@ class Building
       unit.monthly_rent
     end / @units.count.to_f
   end
+
+  def renter_with_highest_rent
+    # iterate through each element of units array
+      # check if there is renter
+    # if there is a renter, find the unit with the highest monthly rent using max_by
+      # return the renter of that unit
+    # if no renter, ignore
+
+    max_rent = 0
+    max_rent = @units.max_by do |unit|
+      if unit.renter
+        binding.pry
+        unit.monthly_rent
+      end
+    end
+
+    max_rent.renter
+    binding.pry
+  end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -3,11 +3,14 @@ require 'minitest/pride'
 require './lib/renter'
 require './lib/apartment'
 require './lib/building'
+require 'pry'
 
 class BuildingTest < Minitest::Test
 
   def setup
     @building = Building.new
+    @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   def test_it_exists
@@ -19,22 +22,29 @@ class BuildingTest < Minitest::Test
   end
 
   def test_it_can_add_units
-    a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
 
-    @building.add_unit(a1)
-    @building.add_unit(b2)
-
-    assert_equal [a1, b2], @building.units
+    assert_equal [@a1, @b2], @building.units
   end
 
   def test_it_has_average_rent
-    a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-    b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-
-    @building.add_unit(a1)
-    @building.add_unit(b2)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
 
     assert_equal 1099.5, @building.average_rent
+  end
+
+  def test_it_can_calculate_highest_renter
+    spencer = Renter.new("Spencer")
+    jessie = Renter.new("Jessie")
+
+    @b2.add_renter(spencer)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+    assert_equal spencer, @building.renter_with_highest_rent
+
+    @a1.add_renter(jessie)
+    assert_equal jessie, @building.renter_with_highest_rent
   end
 end
