271e31f3e90f8b62083e05ed69de3a7d5993cc70
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-28 09:23:39 -0600

    Add tests for attributes and renter in Apartment test

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -1,10 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/apartment'
+require './lib/renter'
 
 
 class ApartmentTest < Minitest::Test
   def setup
+    @jessie = Renter.new("Jessie")
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
   end
 
@@ -12,7 +14,17 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, @a1
   end
 
+  def test_it_has_attributes
+    assert_equal "A1", @a1.number
+    assert_equal 1200, @a1.monthly_rent
+    assert_equal 1, @a1.bathrooms
+    assert_equal 1, @a1.bedrooms
+    assert_nil @a1.renter
+  end
 
-
+  def test_it_can_have_a_renter
+    @a1.add_renter(@jessie)
+    assert_equal @jessie, @a1.renter
+  end
 
 end
