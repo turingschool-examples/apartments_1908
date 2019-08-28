2e5850a824eb32af4eefc4e370da05acdedd8f1a
Michael Cooper <macoop2363@mgmail.com>
2019-08-28 09:50:31 -0600

    Added method functionality

---
+++ lib/building.rb
@@ -0,0 +1,16 @@
+class Building
+  attr_reader :units
+
+  def initialize
+    @units = []
+  end
+
+  def add_unit(unit)
+    @units.push(unit)
+  end
+
+  def average_rent
+    average_rent = @units.sum { |unit| unit.monthly_rent }
+    average_rent /= @units.length.to_f
+  end
+end
