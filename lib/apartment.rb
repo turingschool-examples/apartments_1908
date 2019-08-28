9a5dc44c0153c89980a8d4b7c973828f0684d287
Hillary Stewart <hillarystewart4427@gmail.com>
2019-08-28 09:24:03 -0600

    Add attr_reader and add_renter method

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,8 +1,19 @@
 class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
+
   def initialize(type)
     @number = type[:number]
     @monthly_rent = type[:monthly_rent]
     @bathrooms = type[:bathrooms]
     @bedrooms = type[:bedrooms]
+    @renter = nil
+  end
+
+  def add_renter(person)
+    @renter = person
   end
 end
