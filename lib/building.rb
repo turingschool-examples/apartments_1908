711ade1c0a6418939f198cf3dcc5fe9cad0bd051
Jon Patterson <JonPatt92@gmail.com>
2019-08-28 10:48:40 -0600

    Iteration 3 complete

--- lib/building.rb
+++ lib/building.rb
@@ -7,17 +7,19 @@ class Building
 
   def initialize
     @units = []
-    # @active_units = []
+    @active_units = []
+
   end
 
   def add_unit(unit)
     @units << unit
   end
 
-  # def active_units
-  # @active_units = @units.find_all do |unit|
-  #     unit.renter != nil
-  # end
+  def active_units
+  @active_units = @units.find_all do |unit|
+      unit.renter != nil
+    end
+  end
 
   def average_rent
     total_rent = 0
@@ -26,4 +28,19 @@ class Building
     end
     average_rent = (total_rent.to_f / @units.count.to_f).to_f
   end
+
+  def renter_with_highest_rent
+    active_units
+    highest_rent_unit = @active_units.first
+
+     @active_units.each do |unit|
+      if unit.monthly_rent > highest_rent_unit.monthly_rent
+        highest_rent_unit = unit
+      end
+    end
+
+    highest_rent_unit.renter
+  end
+
+
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -11,10 +11,8 @@ class BuildingTest < Minitest::Test
     @building = Building.new
     @a1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
     @b2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-    # @spencer = Renter.new("Spencer")
-    # @b2.add_renter(@spencer)
-    # @building.add_unit(@a1)
-    # @building.add_unit(@b2)
+    @spencer = Renter.new("Spencer")
+    @b2.add_renter(@spencer)
   end
 
   def test_it_exists
@@ -42,15 +40,19 @@ class BuildingTest < Minitest::Test
     assert_equal 1099.5, @building.average_rent
   end
 
-  # def test_it_knows_highest_rent
-  #
-  #   assert_equal @spencer, @building.renter_with_highest_rent
-  # end
-  #
-  # def test_it_knows_highest_rent_new_renter
-  #   @jessie = Renter.new("Jessie")
-  #   @a1.add_renter(@jessie)
-  #
-  #   assert_equal @jessie, @building.renter_with_highest_rent
-  # end
+  def test_it_knows_highest_rent
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal @spencer, @building.renter_with_highest_rent
+  end
+
+  def test_it_knows_highest_rent_new_renter
+    @jessie = Renter.new("Jessie")
+    @a1.add_renter(@jessie)
+    @building.add_unit(@a1)
+    @building.add_unit(@b2)
+
+    assert_equal @jessie, @building.renter_with_highest_rent
+  end
 end
